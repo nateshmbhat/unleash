42950c11b054dc6ddb64a5b0273545989a584192
Karol Sójko <karolsojko@gmail.com>
2017-06-26 14:34:36 +0200

    Fixed tests

--- config.js
+++ config.js
@@ -3,17 +3,17 @@
  */
 
 module.exports = {
-  skills_api_url: 'skills_api_url_value',
-  goals_api_url: 'goals_api_url_value',
-  profiles_api_url: 'profiles_api_url_value',
-  paths_api_url: 'paths_api_url_value',
+  skills_api_url: 'https://skills_api_url_value',
+  goals_api_url: 'https://goals_api_url_value',
+  profiles_api_url: 'https://profiles_api_url_value',
+  paths_api_url: 'https://paths_api_url_value',
   announcements_api_url: '',
   unleash_app_version: 'local_app_version',
   news_api_url: '',
-  slack_bot_url: 'slack_bot_url_value',
+  slack_bot_url: 'https://slack_bot_url_value',
   firebaseApiKey: 'firebase_api_key_value',
   firebaseAuthDomain: 'firebase_auth_domain_value',
   firebaseDatabaseURL: 'firebase_database_url_value',
-  firebaseStorageBucket: 'firebase_storage_bucket_value',
+  firebaseStorageBucket: 'https://firebase_storage_bucket_value',
   firebaseMessagingSenderId: 'firebase_messaging_sender_id_value',
 };
