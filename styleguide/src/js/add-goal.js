528fe8ed2ae1844cebe010f381762ba378467f27
Agustin G <agiannastasio@gmail.com>
2017-06-30 14:39:14 -0300

    Add Goal modal updates

--- styleguide/src/js/add-goal.js
+++ styleguide/src/js/add-goal.js
@@ -2,9 +2,14 @@
   "use strict";
 	
 	// toggle mobile menu
-	document.querySelector('.js-add-goal').addEventListener("click", function(e){
+	document.querySelector('.js-add-goal__btn').addEventListener("click", function(e){
 		e.preventDefault;
-  	document.querySelector('.js-add-goal-modal').classList.toggle('is-hidden');
+  	document.querySelector('.js-add-goal__overlay').classList.toggle('is-hidden');
+  	document.querySelector('.js-add-goal__modal').classList.toggle('is-hidden');
   });
+	document.querySelector('.js-add-goal__overlay').addEventListener("click", function(e){
+		document.querySelector('.js-add-goal__overlay').classList.add('is-hidden');
+		document.querySelector('.js-add-goal__modal').classList.add('is-hidden');
+	});
 
 })();
--- styleguide/src/templates/objects/o-add-goal.js
+++ styleguide/src/templates/objects/o-add-goal.js
@@ -1,13 +1,18 @@
 module.exports = `
-	<button class="o-add-goal js-add-goal">+</button>
-	<div class="o-add-goal-modal js-add-goal-modal is-hidden">
-		<div class="o-add-goal-modal__content">
-			<p class="o-add-goal-modal__title">Add a Goal</p>
-			<form action="" class="o-add-goal-modal__form">
-				<input type="text" placeholder="Goal name" />
-				<input type="text" placeholder="Goal description" />
+<div class="o-add-goal">
+	<button class="o-add-goal__btn js-add-goal__btn">+</button>
+	<div class="o-add-goal__overlay js-add-goal__overlay is-hidden">
+	</div>
+	<div class="o-add-goal__modal js-add-goal__modal is-hidden">
+		<div class="o-add-goal__modal__content">
+			<p class="o-add-goal__modal__title">Add a Goal</p>
+			<form action="" class="o-add-goal__modal__form">
+				<input class="o-add-goal__modal__input" type="text" placeholder="Goal name">
+				<textarea class="o-add-goal__modal__input" placeholder="Goal description"></textarea>
+				<input type="submit" class="o-add-goal__modal__submit" value="Add">
 			</form>
 		</div>
 	</div>
 	<script src="js/add-goal.js"></script>
+</div>
 `
