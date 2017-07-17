829ddeedde2c81a940d4068f845d012c26a7d9b4
Agustin G <agiannastasio@gmail.com>
2017-07-17 17:47:46 -0300

    Add New Step

--- styleguide/src/js/add-new-step.js
+++ styleguide/src/js/add-new-step.js
@@ -1,7 +1,6 @@
 (function(){
   "use strict";
 	
-	// add new step
 	var buttons = document.querySelectorAll('.js-add-new-step');
 
 	for (var i=0; i<buttons.length; i++){
@@ -16,12 +15,28 @@
 			var newItem = document.createElement('li');
 			newItem.classList.add('timeline__item__checkbox');
 			newItem.innerHTML = `
-				<input class="timeline__item__checkbox__input" type="checkbox">
+				<input class="timeline__item__checkbox__input" type="checkbox" disabled>
 				<input type="text" class="timeline__item__checkbox__edit js-timeline__item__edit">
 			`
 
 			container.insertBefore(newItem, currentItem);
-			document.querySelector('.js-timeline__item__edit').focus();
+			currentItem.previousElementSibling.querySelector('.js-timeline__item__edit').focus();
+			currentItem.classList.add('is-hidden');
+
+			// edit mode
+			document.querySelector('.js-timeline__item__edit').addEventListener("blur", function(e) {
+				var newValue = e.target.value;
+				if (newValue != '') {
+					currentItem.classList.remove('is-hidden');
+					e.target.parentNode.innerHTML = `
+						<input class="timeline__item__checkbox__input" type="checkbox" id="` + newValue.replace(" ", "").toLowerCase() + `">
+						<label class="timeline__item__checkbox__label" for="` + newValue.replace(" ", "").toLowerCase() + `">` + newValue + `</label>
+					`					
+				} else {
+					currentItem.classList.remove('is-hidden');
+					e.target.parentNode.remove();
+				}
+			}, true);
 
 		}, false);
 	}
