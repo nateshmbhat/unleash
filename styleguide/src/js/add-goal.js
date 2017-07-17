27f54d118a442943d91c28235423a9987332d9b5
Agustin G <agiannastasio@gmail.com>
2017-07-17 17:46:32 -0300

    Add Goal details

--- styleguide/src/js/add-goal.js
+++ styleguide/src/js/add-goal.js
@@ -6,6 +6,7 @@
   	document.querySelector('.js-add-goal__overlay').classList.toggle('is-hidden');
   	document.querySelector('.js-add-goal__modal').classList.toggle('is-hidden');
   	document.querySelector('body').classList.toggle('no-scroll');
+  	document.querySelector('.js-add-goal__modal__input').focus();
   });
 	document.querySelector('.js-add-goal__overlay').addEventListener("click", function(e){
 		document.querySelector('.js-add-goal__overlay').classList.add('is-hidden');
