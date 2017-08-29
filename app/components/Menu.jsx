21ee89e33f7e3f404f87dca0c14b38359727310e
Jacek Ławniczak <jacekelgda@gmail.com>
2017-08-29 15:00:05 +0200

    Working tabs menu navigation

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
-import { AppBar, Logo, Avatar, MenuItem, Tabs, Tab, Menu as UnleashStylesMenu } from 'unleash-styles';
 import { routerShape } from 'react-router/lib/PropTypes';
+import { AppBar, Logo, Tab, AvatarButton, Tabs, Menu as UnleashStylesMenu, MenuItem } from 'unleash-styles';
+
 
 class Menu extends Component {
-  handleMenuClick() {
-    console.log('this ...');
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPath: window.location.pathname,
+    };
+  }
+
+  handleMenuClick(path) {
+    this.setState({
+      currentPath: path,
+    });
+    this.props.router.push(path);
+  }
+
+  isCurrent(path) {
+    return this.state.currentPath === path;
   }
 
   render() {
+    const { userId } = this.props;
+
     return (
       <AppBar appBarLogo={<Logo />}>
         <Tabs>
-          <Tab label="home" />
-          <Tab label="my path" />
-          <Tab label="profile" />
-          <Tab label="goals" />
-          <Tab label="skills" />
+          <Tab label="home" isCurrent={this.isCurrent('/')} onClick={() => this.handleMenuClick('/')} />
+          <Tab
+            label="my path"
+            isCurrent={this.isCurrent(`/profiles/${userId}`)}
+            onClick={() => this.handleMenuClick(`/profiles/${userId}`)}
+          />
+          <Tab
+            label="profiles"
+            isCurrent={this.isCurrent('/profiles')}
+            onClick={() => this.handleMenuClick('/profiles')}
+          />
+          <Tab label="goals" isCurrent={this.isCurrent('/goals')} onClick={() => this.handleMenuClick('/goals')} />
+          <Tab label="skills" isCurrent={this.isCurrent('/skills')} onClick={() => this.handleMenuClick('/skills')} />
         </Tabs>
-        <UnleashStylesMenu menuButton={<Avatar type="small" label="NO" />}>
+        <UnleashStylesMenu menuButton={<AvatarButton type="small" label="NO" />}>
           <MenuItem label="nikita" type="important" />
           <MenuItem label="nikita.ormonov@eleken.co" type="email" />
           <MenuItem label="edit profile" type="link" />
@@ -29,6 +55,7 @@ class Menu extends Component {
 }
 
 Menu.propTypes = {
+  userId: React.PropTypes.string.isRequired,
   router: routerShape.isRequired,
 };
 
