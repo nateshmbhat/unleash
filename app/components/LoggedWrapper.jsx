8abd04f90a38a4316b68a373683f5433b3e68c01
Karol Sójko <karolsojko@gmail.com>
2017-09-04 13:06:50 +0200

    Remove left side padding for previous menu

--- app/components/LoggedWrapper.jsx
+++ app/components/LoggedWrapper.jsx
@@ -1,12 +1,10 @@
 import React, { PropTypes } from 'react';
 import Menu from '../containers/Menu';
 
-let styles = {};
-
 const LoggedWrapper = ({ children }) => (
   <div>
     <Menu />
-    <div style={styles.wrapper}>
+    <div>
       {children}
     </div>
   </div>
@@ -14,12 +12,6 @@ const LoggedWrapper = ({ children }) => (
 
 LoggedWrapper.propTypes = {
   children: PropTypes.node,
-};
-
-styles = {
-  wrapper: {
-    padding: '0 0 0 250px',
-  },
 };
 
 export default LoggedWrapper;
